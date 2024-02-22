14fdc3c0e43c1e9f864a1bbca116e6de3e2e50de
girlanasouza <girlanasouza123@gmail.com>
2024-02-22 16:33:49 -0400

    EXE02 - JAVASCRIPT

--- JAVASCRIPT/AULA2/main.js
+++ JAVASCRIPT/AULA2/main.js
@@ -1,99 +1,40 @@
-// 1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
+const displayed_image = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
 
-const customName = document.getElementById('customname');
-const randomize = document.querySelector('.randomize');
-const story = document.querySelector('.story');
+const botao = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
 
+const musicas = ['music1.png', `music2.png`, `music3.png`, `music4.png`, `music5.png`];
 
-function randomValueFromArray(array){
-  const random = Math.floor(Math.random()*array.length);
-  return array[random];
+const album = {
+  'music1.png' : 'Under Pressure - Queen, David Bowie',
+  'music2.png' : 'Enter Sandman - Metallica',
+  'music3.png' : 'Dont\'t Stop Me Now - Queen',
+  'music4.png' : 'Breaking the Law',
+  'music5.png' : 'Everlong - Foo Fighters'
 }
 
-// 2. RAW TEXT STRINGS
 
-const storyText = 'In the land of Middle-earth, the temperature soared to 94 degrees of Elvish heat. And so, the mighty :insertx: embarked on a journey through the Shire. Upon reaching the legendary realm of :inserty:, a moment of awe befell them, followed by a sudden turn of fate — :insertz:. Gandalf witnessed the entire spectacle, his wise eyes twinkling — for :insertx: carried the weight of 300 pounds, and the sun beat fiercely upon the enchanted lands of Middle-earth.';
-const insertX = ['Legolas', 'Bilbon', 'Smaug'];
-const insertY = ['Valfenda', 'Condado', 'Bri'];
-const insertZ = ['coding in java script', 'whispered through the Elven woods', 'soared on the wings of eagles', 'faded like the morning mist','resonated with the Ents laughter'];
+for (const image of musicas) {
+  const nova_imagem = document.createElement('img');
+  nova_imagem.setAttribute('src', `images/${image}`);
+  nova_imagem.setAttribute('alt', album[image]);
+  thumbBar.appendChild(nova_imagem);
+  nova_imagem.addEventListener('click', e => {
+    displayed_image.src = e.target.src;
+    displayed_image.alt = e.target.alt;
+  });
+}
 
-
-// 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
-randomize.addEventListener('click', result);
-
-function result() {
-  const xItem = randomValueFromArray(insertX);
-  const yItem = randomValueFromArray(insertY);
-  const zItem = randomValueFromArray(insertZ);
-
-  let newStory = storyText;
-
-  newStory = newStory.replaceAll(':insertx:', xItem);
-  newStory = newStory.replaceAll(':insertx:', xItem);
-  newStory = newStory.replaceAll(':inserty:', yItem);
-  newStory = newStory.replaceAll(':insertz:', zItem);
-
-  if(customName.value !== '') {
-    const name = customName.value;
-    newStory = newStory.replaceAll('Gandalf', name);
+botao.addEventListener('click', () => {
+  const classe_botao = botao.getAttribute('class');
+  if (classe_botao === 'dark') {
+    botao.setAttribute('class','light');
+    botao.textContent = 'Lighten';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+  } else {
+    botao.setAttribute('class','dark');
+    botao.textContent = 'Darken';
+    overlay.style.backgroundColor = 'rgba(0,0,0,0)';
   }
-
-  if(document.getElementById("uk").checked) {
-    const weight = '${Math.round(300*0.0714286)} stone';
-    const temperature =  '${Math.round((94-32) * 5 / 9)} centigrade';
-    newStory = newStory.replace('94 degrees', temperature);
-    newStory = newStory.replace('300 pounds', weight);
-  }
-
-  story.textContent = newStory;
-  story.style.visibility = 'visible';
-}
-
-
-/*
-ideias de projeto com javascript
-declaração de uma função
-const imprimir = (str) => {
-  console.log(str);
-}
-
-
-const display = str => console.log(str)       // recebe uma string 
-const sum = (a,b) => a + b
-
-design pattern book: design patterns building extensible e maintainable oo softtware  authors: eric freeman an elisaeth
-
-const hello = function {
-  return "hello word";
-}
-
-const hello = function {
-  return function () {
-    return "hello word";
-  }
-}
-
--more simple sintax
-
-funcao de primeira classe - carrinho de compra - algum tipo de desconto - adicionar desconto e funcao q verfica se o desconto 
-eh valido
-
-
-executar prog = node index.js
-
-let funciona apenas dentro da chave 
-
-function foo (){
-  if(true){
-    leb b="ufam";
-    document.writeln(typeod b); // string
-  }
-  document.writeln(typeof b); // undefined
-}
-
-const - n permite alterações
-
-
-
-
-*/
+});

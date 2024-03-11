027c381407c828207d5851e11f7b50c46de90fec
girlanasouza <girlanasouza123@gmail.com>
2024-03-11 08:18:31 -0400

    add exe1

--- NODEJS/node1/node1.js
+++ NODEJS/node1/node1.js
@@ -1,28 +1,43 @@
 
 /* 
-
+Usando a função readdir do módulo fs, desenvolva um programa
+que aceita o nome de um diretório como parâmetro, e então cria
+um servidor Web capaz de retornar uma página contendo a lista de
+arquivos e subdiretórios do diretório informado
 */
-const http = require('http');
 
-const files = ['file1.txt', 'file2.txt', 'file3.txt', 'file4.txt']
+
+/* PARTE 1*/
+const http = require('http');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3333;
+const fs = require('fs');
 
 const server = http.createServer(function(req,res){
     const directory = process.argv[2];
     fs.readdir(directory, function(err, files) {
-        if (err) {
-            res.writeHead(500, {"Content-Type": "text/plain"});
-            res.end(`Erro ao ler o diretório: ${err}`);
-            return;
-        }
         res.writeHead(200,{"Content-Type":"text/html;charset=utf-8"});
         files.forEach(file => {
-            res.write(`${file}<br>`) 
+            res.write(`${file}<br>`); 
         });
         res.end();
     });
 });
 
-const port = 3333;
-server.listen(port, function() {
-    console.log(`Servidor rodando em http://localhost:${port}/`);
-});
+server.listen(PORT);
+
+/* PARTE 2*/
+
+// const http = require('http');
+// require('dotenv').config();
+
+// const PORT = process.env.PORT || 3333;
+
+// const server = http.createServer(function (req, res) {
+//     res.writeHead(200, {"Content-Type":"text/html;charset=utf-8"});
+//     res.write("Instituto de Computação");
+//     res.end();
+// });
+
+// server.listen(PORT);

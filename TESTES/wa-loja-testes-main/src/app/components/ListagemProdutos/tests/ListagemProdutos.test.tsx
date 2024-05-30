6350c1a106bf4211503eb474eccacee98efea47a
girlanasouza <girlanasouza123@gmail.com>
2024-05-30 16:34:03 -0400

    adicionando parte dos testes de integração

--- TESTES/wa-loja-testes-main/src/app/components/ListagemProdutos/tests/ListagemProdutos.test.tsx
+++ TESTES/wa-loja-testes-main/src/app/components/ListagemProdutos/tests/ListagemProdutos.test.tsx
@@ -1,17 +1,53 @@
 import { screen, render } from "@testing-library/react";
 
 import ListagemProdutos from "../ListagemProdutos";
-import { useFavoritosContext } from "@/app/State/FavoritosProvider";
+import {
+  FavoritosProvider,
+  useFavoritosContext,
+} from "@/app/State/FavoritosProvider";
+import { mockProdutos } from "@/app/mocks/produtos";
+import userEvent from "@testing-library/user-event";
 
-jest.mock("@/app/State/FavoritosProvider"); 
+jest.mock("../../../State/FavoritosProvider", () => ({
+  ...jest.requireActual("../../../State/FavoritosProvider"),
+  useFavoritosContext: jest.fn(),
+}));
 
-describe("ListagemProdutos", () => {{
-    it("deve rendenrizar corretamente a pagina de listagem de produtos", () => {
-        const useListagemProdutosMock = useFavoritosContext as jest.Mock;
-        useListagemProdutosMock.mockReturnValue(false);
-        render(
-            <ListagemProdutos produtos={[]}/>
-        );
-        
-    })
-}})
+describe("ListagemProdutos", () => {
+  it("deve rendenrizar corretamente a listagem de produtos", () => {
+    const setFavoritos = jest.fn();
+    const useListagemProdutosMock = useFavoritosContext as jest.Mock;
+    useListagemProdutosMock.mockReturnValue({ setFavoritos });
+
+    render(
+      <FavoritosProvider>
+        <ListagemProdutos produtos={[]} />
+      </FavoritosProvider>
+    );
+
+    expect(screen.getByText("Produtos disponíveis:")).toBeInTheDocument();
+    mockProdutos.forEach((produto) => {
+      expect(screen.getByText(produto.nome)).toBeInTheDocument();
+      expect(screen.getByText(`De R$ ${produto.preco}`)).toBeInTheDocument();
+    });
+  });
+
+  it("deve chamar a função setFavoritos ao adicionar produto aos favoritos", async () => {
+    const setFavoritos = jest.fn();
+    const useFavoritosContextMock = useFavoritosContext as jest.Mock;
+    useFavoritosContextMock.mockReturnValue({ setFavoritos });
+
+    render(
+      <FavoritosProvider>
+        <ListagemProdutos produtos={mockProdutos} />
+      </FavoritosProvider>
+    );
+
+    const botaoAdicionar = screen.getAllByRole("button", {
+      name: /Adicionar aos favoritos/i,
+    })[0];
+    await userEvent.click(botaoAdicionar);
+
+    expect(setFavoritos).toHaveBeenCalledTimes(1);
+  });
+});
